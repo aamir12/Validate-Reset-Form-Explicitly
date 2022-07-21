7b11c2aa933fdbef9948eda20a08b3fd390909cd
aamir12 <aamirkhan8878@gmail.com>
2022-07-21 16:27:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-jsk5cq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-jsk5cq)
---
+++ src/app/app.component.ts
@@ -0,0 +1,113 @@
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  singupForm: FormGroup;
+  ngOnInit() {
+    this.singupForm = new FormGroup({
+      userdata: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      gender: new FormControl(null, Validators.required),
+      classmates: new FormArray([
+        new FormControl('', [Validators.required]),
+        new FormControl('', [Validators.required]),
+      ]),
+
+      addresses: new FormArray([
+        new FormGroup({
+          street: new FormControl(null, Validators.required),
+          city: new FormControl(null, Validators.required),
+        }),
+      ]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.singupForm.value);
+    if (this.singupForm.valid) {
+      console.log('form submitted');
+    } else {
+      this.validateAllFormFields(this.singupForm);
+    }
+  }
+
+  classMate(i) {
+    return (<FormArray>this.singupForm.get('classmates')).controls[i];
+  }
+
+  city(i) {
+    return (<FormArray>this.singupForm.get('addresses')).controls[i].get(
+      'city'
+    );
+  }
+
+  street(i) {
+    return (<FormArray>this.singupForm.get('addresses')).controls[i].get(
+      'street'
+    );
+  }
+
+  get classmates() {
+    return this.singupForm.get('classmates') as FormArray;
+  }
+
+  get addresses() {
+    return this.singupForm.get('addresses') as FormArray;
+  }
+
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      } else if (control instanceof FormArray) {
+        for (let c of control.controls) {
+          if (c instanceof FormControl) {
+            c.markAsTouched({ onlySelf: true });
+          } else if (c instanceof FormGroup) {
+            this.validateAllFormFields(c);
+          }
+        }
+      }
+    });
+  }
+
+  resetForm(formGroup: FormGroup) {
+    formGroup.reset();
+    //return  reset only work but sometimes need
+    // extra effort
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        this.resetControl(control);
+      } else if (control instanceof FormGroup) {
+        this.resetForm(control);
+      } else if (control instanceof FormArray) {
+        for (let c of control.controls) {
+          if (c instanceof FormControl) {
+            c.markAsTouched({ onlySelf: true });
+          } else if (c instanceof FormGroup) {
+            this.resetForm(c);
+          }
+        }
+      }
+    });
+  }
+
+  resetControl(control: FormControl) {
+    console.log(control);
+    control.markAsPristine();
+    control.markAsUntouched();
+    //control.setErrors(null);
+  }
+}
